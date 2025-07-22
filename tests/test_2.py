bbadd8929b9d3c32440faecbefcc6a674179078d
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-07-22 11:37:26 -0400

    Add/update test_2.py file

--- tests/test_2.py
+++ tests/test_2.py
@@ -1,15 +1,30 @@
 import pytest
-from definition_3aa83a58bf214bb3b3e5ce5be7466892 import calculate_aggregate_risks
+import pandas as pd
+from definition_7fac5ad143d24f9eb5872c4db8564af9 import calculate_retained_loss
 
-@pytest.mark.parametrize("gross_losses, transferred_losses, expected", [
-    ([100, 200, 300], [10, 20, 30], (600, 540, 60)),
-    ([50, 50, 50], [0, 0, 0], (150, 150, 0)),
-    ([10, 20, 30], [10, 20, 30], (60, 0, 60)),
-    ([100], [50], (100, 50, 50)),
-    ([], [], (0, 0, 0)),
-])
-def test_calculate_aggregate_risks(gross_losses, transferred_losses, expected):
-    gross_risk, net_risk, transferred_risk = calculate_aggregate_risks(gross_losses, transferred_losses)
-    assert gross_risk == expected[0]
-    assert net_risk == expected[1]
-    assert transferred_risk == expected[2]
+@pytest.fixture
+def sample_series():
+    return pd.Series([100, 200, 300, 400, 500])
+
+def test_calculate_retained_loss_typical(sample_series):
+    payout_series = pd.Series([10, 20, 30, 40, 50])
+    expected_retained_loss = pd.Series([90, 180, 270, 360, 450])
+    pd.testing.assert_series_equal(calculate_retained_loss(sample_series, payout_series), expected_retained_loss)
+
+def test_calculate_retained_loss_empty_series():
+    gross_loss_series = pd.Series([])
+    payout_series = pd.Series([])
+    expected_retained_loss = pd.Series([])
+    pd.testing.assert_series_equal(calculate_retained_loss(gross_loss_series, payout_series), expected_retained_loss)
+
+def test_calculate_retained_loss_zero_payout(sample_series):
+    payout_series = pd.Series([0, 0, 0, 0, 0])
+    pd.testing.assert_series_equal(calculate_retained_loss(sample_series, payout_series), sample_series)
+
+def test_calculate_retained_loss_full_payout(sample_series):
+    pd.testing.assert_series_equal(calculate_retained_loss(sample_series, sample_series), pd.Series([0, 0, 0, 0, 0]))
+
+def test_calculate_retained_loss_mismatched_lengths(sample_series):
+    payout_series = pd.Series([10, 20, 30])
+    with pytest.raises(ValueError, match="operands could not be broadcast together with shapes"):
+        calculate_retained_loss(sample_series, payout_series)
